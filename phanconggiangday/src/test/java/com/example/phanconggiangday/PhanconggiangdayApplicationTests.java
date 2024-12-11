d1e8e0125be1e7eca2d477b43e590500514da95b
Thang05 <114286940+Thang05@users.noreply.github.com>
2024-12-11 17:07:35 +0700

    Add files via upload

---
+++ phanconggiangday/src/test/java/com/example/phanconggiangday/PhanconggiangdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phanconggiangday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanconggiangdayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
